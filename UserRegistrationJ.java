7fd9627c9736b35f673a2c8c37de90535d738929
redBOY <43438363+redBOY@users.noreply.github.com>
2022-09-18 19:42:00 +0530

    UC2

---
+++ UserRegistrationJ.java
@@ -0,0 +1,13 @@
+package combridgelabz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationJ {
+    public boolean lastName(String lastName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
+}
