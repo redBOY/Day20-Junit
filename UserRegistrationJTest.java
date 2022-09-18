1819963be40473a07a95baf97b2f6843217557e9
redBOY <43438363+redBOY@users.noreply.github.com>
2022-09-18 20:17:18 +0530

    UC4

--- UserRegistrationJTest.java
+++ UserRegistrationJTest.java
@@ -9,13 +9,13 @@ public class UserRegistrationJTest {
     UserRegistrationJ userRegistration = new UserRegistrationJ();
     @Test
     public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
-        boolean result = userRegistration.phoneNumber("91 9538639621");
+        boolean result = userRegistration.phoneNumber("91 9731760530");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenPhoneNumber_WhenNotProper_ShouldReturnFlase() {
-        boolean result = userRegistration.phoneNumber("919538639621");
+        boolean result = userRegistration.phoneNumber("919731760530");
         Assert.assertEquals(false, result);
     }
 }
