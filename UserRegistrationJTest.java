9cd122873058bd0d92cd5d834cfbeabc4b974b8f
redBOY <43438363+redBOY@users.noreply.github.com>
2022-11-17 09:20:18 +0530

    UC1

--- UserRegistrationJTest.java
+++ UserRegistrationJTest.java
@@ -9,13 +9,13 @@ public class UserRegistrationJTest {
     UserRegistrationJ userRegistration = new UserRegistrationJ();
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
-        boolean result = userRegistration.firstName("Shashi");
+        boolean result = userRegistration.firstName("Adithya");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
-        boolean result = userRegistration.firstName("shashi");
+        boolean result = userRegistration.firstName("Adithya");
         Assert.assertEquals(false, result);
     }
 
